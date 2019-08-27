719a97505a0473a3a3e9611e3f92f081921882cb
Mikian Musser <Bobar312@gmail.com>
2019-08-27 12:31:51 -0700

    Submitting General Scripts

---
+++ reducing_big_images.py
@@ -0,0 +1,35 @@
+## reducing_big_images.py ##
+## Augment dataset with crops of images of a set size ##
+## while preserving the xml data                      ##
+#
+# Authors:
+#   Mikian Musser - https://github.com/mm909
+#
+# Usage:
+#	python reducing_big_images.py
+#
+# Todo:
+#	Take directories in as arguments
+#
+
+
+import os
+from impy.ObjectDetectionDataset import ObjectDetectionDataset
+
+def main():
+	# Define the path to images and annotations
+	images_path = os.path.join(os.getcwd(), "images", "train", "image")
+	annotations_path = os.path.join(os.getcwd(), "images", "train", "xml")
+	# Define the name of the dataset
+	dbName = "CardsDataset"
+	# Create an object of ObjectDetectionDataset
+	obda = ObjectDetectionDataset(imagesDirectory=images_path, annotationsDirectory=annotations_path, databaseName=dbName)
+	# Reduce the dataset to smaller Rois of smaller ROIs of shape 1032x1032.
+	# offset=[1032, 1032]
+	offset=[300, 300]
+	images_output_path = os.path.join(os.getcwd(), "reducedImages", "train")
+	annotations_output_path = os.path.join(os.getcwd(), "reducedImages", "train")
+	obda.reduceDatasetByRois(offset = offset, outputImageDirectory = images_output_path, outputAnnotationDirectory = annotations_output_path)
+
+if __name__ == "__main__":
+	main()
---
+++ rename.py
@@ -0,0 +1,29 @@
+## rename.py ##
+## Rename files with .JPG to .jpg ##
+#
+# Author:
+#   Mikian Musser - https://github.com/mm909
+#
+# Usage:
+#   python rename.py {Path to dir}/*
+#
+
+import glob
+import os
+import sys
+
+print("Active:", str(sys.argv[1]))
+images = glob.glob(str(sys.argv[1]))
+print("Total Images:", len(images))
+
+count = 0
+
+for file in images:
+    src = file.replace('\\', '/')
+    extension = os.path.splitext(file)[1]
+    if(extension != '.py' and extension != '.jpg'):
+        dst = os.path.splitext(file)[0] + '.jpg'
+        os.rename(src, dst)
+        count = count + 1
+
+print("Images renamed:", count)
